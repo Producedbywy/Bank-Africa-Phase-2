bf46165328b2b0da959af70eedf61f7db9203020
Producedbywy <producedbywy@icloud.com>
2025-07-01 12:50:40 +0000

    Update build.sh after moving project to repo root

--- build.sh
+++ build.sh
@@ -6,9 +6,6 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Move into the Django project folder
-cd Bank-Africa-django--main
-
 # Run migrations and collect static files
 python manage.py migrate
 python manage.py collectstatic --noinput
